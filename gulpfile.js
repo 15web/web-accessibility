62820656978034b7e997c77f311d618e85598a32
Vodopyanov Egor <headfire94work@gmail.com>
2016-05-26 13:39:29 +0600

    cssComb

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,98 @@
 'use strict';
 
 const gulp = require('gulp');
+const gulpsync = require('gulp-sync')(gulp);
 const gp = require('gulp-load-plugins')();
-const sass = require('gulp-sass');
-const debug = require('gulp-debug');
 const webpack = require('webpack');
-const gutil = require('gulp-util');
-const rename = require("gulp-rename");
-const uglify = require('gulp-uglify');
-const cssnano = require('gulp-cssnano');
-
+const gutil = require('gulp-util'); // gp.gutil не работает в вебпаке
 const browserSync = require('browser-sync').create();
 
-var DEBUG = false;
+var production = gp.util.env.production || false;
+
+
+var settings = {
+    path: {
+        example: './examples/sp',
+        default: './src',
+        build: './dist'
+    }
+};
 
 // examples-sp only
 
 gulp.task('html', function () {
-    gulp.src('./examples/sp/twig/*.twig')
+    gulp.src(settings.path.example + '/twig/*.twig')
         .pipe(gp.cached('html'))
-        .pipe(gp.twig())
-        .on('error', console.log)
-        .pipe(gulp.dest('./examples/sp/'))
+        .pipe(gp.twig().on('error', console.log))
+        .pipe(gulp.dest(settings.path.example))
 });
 
 gulp.task("sp", function () {
-    return gulp.src('./examples/sp/assets/styles/full.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./examples/sp/assets/styles/css'));
+    return gulp.src(settings.path.example + '/assets/styles/full.scss')
+        .pipe(gp.cached('sp'))
+        .pipe(gp.sass().on('error', gp.sass.logError))
+        .pipe(gulp.dest(settings.path.example + '/assets/styles/css'));
 });
 
 gulp.task('scripts', function () {
-        return gulp.src('./dist/accessibility.js')
-            .pipe(uglify())
-            .pipe(rename('accessibility.min.js'))
-            .pipe(gulp.dest('./dist'));
+    return gulp.src(settings.path.build + '/accessibility.js')
+        .pipe(gp.cached('scripts'))
+        .pipe(gp.uglify())
+        .pipe(gp.rename('accessibility.min.js'))
+        .pipe(gulp.dest(settings.path.build));
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./dist/accessibility.css')
-        .pipe(cssnano())
-        .pipe(rename('accessibility.min.css'))
-        .pipe(gulp.dest('./dist'));
+    return gulp.src(settings.path.build + '/accessibility.css')
+        .pipe(gp.cached('styles'))
+        .pipe(gp.csscomb())
+        .pipe(gulp.dest(settings.path.build))
+        .pipe(gp.cssnano())
+        .pipe(gp.rename('accessibility.min.css'))
+        .pipe(gulp.dest(settings.path.build));
 });
 
 gulp.task("webpack", function (callback) {
 
-    var flag = true;
-    var configFile = './webpack.config.js';
-    var configFileContent = require(configFile);
-    var finalConfig = Object.assign({}, configFileContent);
-    var webpackStatsConfig = {
-        colors: true, hash: false, version: false, timings: false, assets: true, chunks: false,
-        chunkModules: false, modules: false, children: false, cached: false, reasons: false,
-        source: false, errorDetails: false, chunkOrigins: false
-    };
-    webpack(finalConfig, function (err,stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        var title = 'webpack' + ' compile #'+':\r\n';
-        gutil.log(title, stats.toString(webpackStatsConfig));
+    var firstBuild = true;
+    var configFile = require('./webpack.config.js');
+    webpack(configFile, function (err, stats) {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log(stats.toString({
+            colors: true, 
+            hash: false,
+            chunks: false,
+            children: false
 
-        if (flag) {
+        }));
+
+        if (firstBuild) {
             callback();
-            flag = false;
+            firstBuild = false;
         }
 
     });
 });
 
-gulp.task('default', ['webpack', 'html', 'sp', 'server'], function () {
-    gulp.watch('./examples/sp/twig/**/*.twig', ['html']);
-    gulp.watch('./examples/sp/assets/styles/**/*.scss', ['sp']);
+gulp.task('dev', gulpsync.sync([['webpack', 'html', 'sp'], ['server']]), function () {
+    gulp.watch(settings.path.example + '/twig/**/*.twig', ['html']);
+    gulp.watch(settings.path.example + '/assets/styles/**/*.scss', ['sp']);
 });
 
-gulp.task('production', ['webpack', 'html', 'sp','styles','scripts','server'], function () {
-    gulp.watch('./examples/sp/twig/**/*.twig', ['html']);
-    gulp.watch('./examples/sp/assets/styles/**/*.scss', ['sp']);
-});
+if (production) {
+    gulp.task('default', gulpsync.sync(['dev', 'scripts', 'styles']));
+}
+else {
+    gulp.task('default', ['dev']);
+}
+
 
 gulp.task('server', function () {
     browserSync.init({
         server: './',
         open: false,
-        startPath: "/examples/sp/index.html"
+        startPath: settings.path.example + "/index.html"
     });
-    gulp.watch('./dist/**/*').on('change', browserSync.reload);
-    gulp.watch('./examples/sp/*.html').on('change', browserSync.reload);
+    gulp.watch(settings.path.build + '/**/*').on('change', browserSync.reload);
+    gulp.watch(settings.path.example + '/*.html').on('change', browserSync.reload);
 });
