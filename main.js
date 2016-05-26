c682bdde22bf79b999e6fc9ee882c754a9be666a
Vodopyanov Egor <headfire94work@gmail.com>
2016-05-26 15:34:59 +0600

    merge script

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
 $(document).ready(function () {
-    var panel1 = new tabpanel("tabpanel1", false); //табы
-    var hs1 = new hideShow('button1'); //кнопка скрыть показать
+    var panel1 = new accessibility.tabpanel("tabpanel1", false); //табы
+    var hs1 = new accessibility.hideShow('button1'); //кнопка скрыть показать
 });
