51a9366dba322caf7ce4df189e21daa598bc2f54
Karavaev Evgeniy <karavaev.evgeniy@gmail.com>
2016-05-30 14:43:38 +0700

    Правки структуры

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,10 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-
-
 gulp.task('server', function () {
     browserSync.init({
         server: './',
-        open: false,
+        open: false
     });
     gulp.watch('*.html').on('change', browserSync.reload);
 });
