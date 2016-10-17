5b49a05ebd8f16691ca3beb6e5863a9af0b41208
Vodopyanov Egor <headfire94work@gmail.com>
2016-10-17 09:24:39 +0700

    панель управления - только верстка и дропдаун

--- dist/accessibility.js
+++ dist/accessibility.js
@@ -137,9 +137,6 @@ var accessibility =
 
 	'use strict';
 	
-	// import cookie from '../cookie';
-	// const COOKIE_PATH = {path: '/'};
-	
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
@@ -154,202 +151,46 @@ var accessibility =
 	    function ControlPanel() {
 	        _classCallCheck(this, ControlPanel);
 	
-	        this.app = $('html');
-	        this.options = {
-	            mainControls: {
-	                1: 'fontSize',
-	                2: 'letterSpacing',
-	                3: 'color',
-	                4: 'img'
-	            },
-	            extraMenu: true,
-	            containerId: 'accessibility-menu',
-	            controlBtn: 'accessibility-menu__switcher-btn'
-	        };
-	
-	        this.controlPanel = document.getElementById(this.options.containerId);
-	        this.controls = document.querySelectorAll('accessibility-menu__switcher-btn');
-	        // this.dropdownBtn = this.controlPanel.find('#dropdownBtn');
-	        // this.dropdown = this.controlPanel.find('#dropdownMenu');
-	        // this.handleControlClick();
-	        // this.handleDropdown();
-	        // self.app = $('.application');
-	        //
-	        // self.textSelect = $('.control-panel__select-text');
-	        // self.colorSelect = $('.control-panel__select-color');
-	        // self.imageSelect = $('.control-panel__select-image');
-	
-	        // !cookie.getCookie('app-text') ? cookie.setCookie('app-text', 'normal', COOKIE_PATH) : false;
-	        // !cookie.getCookie('app-color') ? cookie.setCookie('app-color', 'white', COOKIE_PATH) : false;
-	        // !cookie.getCookie('app-image') ? cookie.setCookie('app-image', 'visible', COOKIE_PATH) : false;
-	
+	        this.controlPanel = document.getElementById('accessibility-menu');
+	        this.dropdownBtn = document.getElementById('dropdownBtn');
+	        this.dropdown = document.getElementById('dropdownMenu');
+	        this.init();
 	    }
 	
 	    _createClass(ControlPanel, [{
 	        key: 'init',
-	        value: function init() {}
-	    }, {
-	        key: 'handleControlClick',
-	        value: function handleControlClick() {
-	            var _this = this;
-	
-	            this.controls.on('click', function (e) {
-	                e.preventDefault();
-	                var data = $(e.target).data();
-	                _this.setAppStyle(data);
-	            });
+	        value: function init() {
+	            this.handleDropdown();
 	        }
 	    }, {
 	        key: 'handleDropdown',
 	        value: function handleDropdown() {
-	            var _this2 = this;
+	            var _this = this;
 	
 	            this.setDefaultDropdown();
-	            this.dropdownBtn.on('click', function (e) {
+	            this.dropdownBtn.addEventListener('click', function (e) {
 	                e.preventDefault();
-	                if (_this2.dropdownBtn.attr('aria-expanded') == 'false') {
-	                    _this2.dropdownBtn.attr('aria-expanded', 'true');
-	                    _this2.controlPanel.addClass('new-control-panel_show-dropdown');
-	                    _this2.dropdown.attr('aria-hidden', 'false');
-	                    _this2.dropdown.attr('aria-expanded', 'true');
+	                if (_this.dropdownBtn.getAttribute('aria-expanded') == 'false') {
+	                    _this.dropdownBtn.setAttribute('aria-expanded', 'true');
+	                    _this.controlPanel.classList.add('accessibility-menu_show-dropdown');
+	                    _this.dropdown.setAttribute('aria-hidden', 'false');
+	                    _this.dropdown.setAttribute('aria-expanded', 'true');
 	                } else {
-	                    _this2.dropdownBtn.attr('aria-expanded', 'false');
-	                    _this2.controlPanel.removeClass('new-control-panel_show-dropdown');
-	                    _this2.dropdown.attr('aria-hidden', 'true');
-	                    _this2.dropdown.attr('aria-expanded', 'false');
+	                    _this.dropdownBtn.setAttribute('aria-expanded', 'false');
+	                    _this.controlPanel.classList.remove('accessibility-menu_show-dropdown');
+	                    _this.dropdown.setAttribute('aria-hidden', 'true');
+	                    _this.dropdown.setAttribute('aria-expanded', 'false');
 	                }
 	            });
 	        }
 	    }, {
 	        key: 'setDefaultDropdown',
 	        value: function setDefaultDropdown() {
-	            this.dropdownBtn.attr('aria-haspopup', 'true');
-	            this.dropdownBtn.attr('aria-expanded', 'false');
-	            this.dropdown.attr('aria-expanded', 'false');
-	            this.dropdown.attr('aria-hidden', 'true');
+	            this.dropdownBtn.setAttribute('aria-haspopup', 'true');
+	            this.dropdownBtn.setAttribute('aria-expanded', 'false');
+	            this.dropdown.setAttribute('aria-expanded', 'false');
+	            this.dropdown.setAttribute('aria-hidden', 'true');
 	        }
-	    }, {
-	        key: 'setAppStyle',
-	        value: function setAppStyle(data) {
-	            switch (data.type) {
-	
-	                case 'font-size':
-	                    {
-	                        if (data.increase) {
-	                            this.app.css(data.type, '+=1px');
-	                        } else {
-	                            this.app.css(data.type, '-=1px');
-	                        }
-	                        break;
-	                    }
-	                case 'letter-spacing':
-	                    {
-	                        if (data.increase) {
-	                            this.app.css(data.type, '+=0.025em');
-	                        } else {
-	                            this.app.css(data.type, '-=0.025em');
-	                        }
-	                        break;
-	                    }
-	
-	                case 'img':
-	                    {
-	                        if (data.view) {
-	                            this.app.find('img').css('visibility', 'visible');
-	                        } else {
-	                            this.app.find('img').css('visibility', 'hidden');
-	                        }
-	                        break;
-	                    }
-	            }
-	        }
-	
-	        // handleTextChange() {
-	        //
-	        //     var self = this;
-	        //
-	        //     self.textSelect.val(cookie.getCookie('app-text'));
-	        //
-	        //     self.textSelect.on('change', function() {
-	        //
-	        //         cookie.setCookie('app-text', $(this).val(), COOKIE_PATH);
-	        //         self.setApplicationStyles();
-	        //
-	        //     });
-	        //
-	        // }
-	        //
-	        // handleColorChange() {
-	        //
-	        //     var self = this;
-	        //
-	        //     self.colorSelect.val(cookie.getCookie('app-color'));
-	        //
-	        //     self.colorSelect.on('change', function() {
-	        //
-	        //         cookie.setCookie('app-color', $(this).val(), COOKIE_PATH);
-	        //         self.setApplicationStyles();
-	        //
-	        //     });
-	        //
-	        // }
-	        //
-	        // handleImageChange() {
-	        //
-	        //     var self = this;
-	        //
-	        //     self.imageSelect.val(cookie.getCookie('app-image'));
-	        //
-	        //     self.imageSelect.on('change', function() {
-	        //
-	        //         cookie.setCookie('app-image', $(this).val(), COOKIE_PATH);
-	        //         self.setApplicationStyles();
-	        //
-	        //         self.toggleImage();
-	        //
-	        //     });
-	        //
-	        //
-	        //
-	        // }
-	        //
-	        // wrapImageAlt() {
-	        //
-	        //     $('img').each(function() {
-	        //
-	        //         var alt = $(this).attr('alt');
-	        //
-	        //         if (alt != '') {
-	        //             $(this).wrap(`<span class="image-alt"/>`);
-	        //             $(this).after(`<span class="image-alt__text">${alt}</span>`);
-	        //         } else {
-	        //             $(this).wrap(`<span class="image-alt image-alt_empty"/>`);
-	        //         }
-	        //
-	        //     });
-	        //
-	        // }
-	        //
-	        // toggleImage() {
-	        //
-	        //     if (cookie.getCookie('app-image') == 'hidden') {
-	        //         $('.image-alt').addClass('image-alt_active');
-	        //         $('img').css('visibility', 'hidden');
-	        //     } else {
-	        //         $('.image-alt').removeClass('image-alt_active');
-	        //         $('img').css('visibility', 'visible');
-	        //     }
-	        //
-	        // }
-	        //
-	        // setApplicationStyles() {
-	        //
-	        //     this.app.attr('data-text', cookie.getCookie('app-text'));
-	        //     this.app.attr('data-color', cookie.getCookie('app-color'));
-	        //     this.app.attr('data-image', cookie.getCookie('app-image'));
-	        //
-	        // }
-	
 	    }]);
 	
 	    return ControlPanel;
--- src/components/default/accessibility-menu/index.js
+++ src/components/default/accessibility-menu/index.js
@@ -1,200 +1,42 @@
-'use strict';
-
-// import cookie from '../cookie';
-// const COOKIE_PATH = {path: '/'};
 import './accessibility-menu.scss';
 
 class ControlPanel {
     constructor() {
-        this.app = $('html');
-        this.options = {
-            mainControls : {
-                1 : 'fontSize',
-                2 : 'letterSpacing',
-                3 : 'color',
-                4: 'img'
-            },
-            extraMenu : true,
-            containerId : 'accessibility-menu',
-            controlBtn : 'accessibility-menu__switcher-btn'
-        };
-
-        this.controlPanel = document.getElementById(this.options.containerId);
-        this.controls = document.querySelectorAll('accessibility-menu__switcher-btn');
-        // this.dropdownBtn = this.controlPanel.find('#dropdownBtn');
-        // this.dropdown = this.controlPanel.find('#dropdownMenu');
-        // this.handleControlClick();
-        // this.handleDropdown();
-        // self.app = $('.application');
-        //
-        // self.textSelect = $('.control-panel__select-text');
-        // self.colorSelect = $('.control-panel__select-color');
-        // self.imageSelect = $('.control-panel__select-image');
-
-        // !cookie.getCookie('app-text') ? cookie.setCookie('app-text', 'normal', COOKIE_PATH) : false;
-        // !cookie.getCookie('app-color') ? cookie.setCookie('app-color', 'white', COOKIE_PATH) : false;
-        // !cookie.getCookie('app-image') ? cookie.setCookie('app-image', 'visible', COOKIE_PATH) : false;
-
-
+        this.controlPanel = document.getElementById('accessibility-menu');
+        this.dropdownBtn = document.getElementById('dropdownBtn');
+        this.dropdown = document.getElementById('dropdownMenu');
+        this.init();
     }
 
     init() {
-
+        this.handleDropdown();
     }
 
-    handleControlClick() {
-        this.controls.on('click', (e)=> {
-            e.preventDefault();
-            var data = $(e.target).data();
-            this.setAppStyle(data);
-        })
-    }
     handleDropdown() {
         this.setDefaultDropdown();
-        this.dropdownBtn.on('click' , (e)=> {
+        this.dropdownBtn.addEventListener('click' , (e)=> {
             e.preventDefault();
-            if(this.dropdownBtn.attr('aria-expanded') == 'false') {
-                this.dropdownBtn.attr('aria-expanded', 'true');
-                this.controlPanel.addClass('new-control-panel_show-dropdown');
-                this.dropdown.attr('aria-hidden','false');
-                this.dropdown.attr('aria-expanded','true');
+            if(this.dropdownBtn.getAttribute('aria-expanded') == 'false') {
+                this.dropdownBtn.setAttribute('aria-expanded', 'true');
+                this.controlPanel.classList.add('accessibility-menu_show-dropdown');
+                this.dropdown.setAttribute('aria-hidden','false');
+                this.dropdown.setAttribute('aria-expanded','true');
             }
             else {
-                this.dropdownBtn.attr('aria-expanded', 'false');
-                this.controlPanel.removeClass('new-control-panel_show-dropdown');
-                this.dropdown.attr('aria-hidden','true');
-                this.dropdown.attr('aria-expanded','false');
+                this.dropdownBtn.setAttribute('aria-expanded', 'false');
+                this.controlPanel.classList.remove('accessibility-menu_show-dropdown');
+                this.dropdown.setAttribute('aria-hidden','true');
+                this.dropdown.setAttribute('aria-expanded','false');
             }
         });
     }
+
     setDefaultDropdown() {
-        this.dropdownBtn.attr('aria-haspopup', 'true');
-        this.dropdownBtn.attr('aria-expanded', 'false');
-        this.dropdown.attr('aria-expanded','false');
-        this.dropdown.attr('aria-hidden','true');
+        this.dropdownBtn.setAttribute('aria-haspopup', 'true');
+        this.dropdownBtn.setAttribute('aria-expanded', 'false');
+        this.dropdown.setAttribute('aria-expanded','false');
+        this.dropdown.setAttribute('aria-hidden','true');
     }
-
-    setAppStyle(data) {
-        switch (data.type) {
-
-            case 'font-size' : {
-                if(data.increase) {
-                    this.app.css(data.type, '+=1px')
-                }
-                else {
-                    this.app.css(data.type, '-=1px')
-                }
-                break;
-            }
-            case 'letter-spacing' : {
-                if(data.increase) {
-                    this.app.css(data.type, '+=0.025em')
-                }
-                else {
-                    this.app.css(data.type, '-=0.025em')
-                }
-                break;
-            }
-
-            case 'img' : {
-                if (data.view) {
-                    this.app.find('img').css('visibility','visible');
-                }
-                else {
-                    this.app.find('img').css('visibility','hidden');
-                }
-                break;
-            }
-        }
-
-
-    }
-
-    // handleTextChange() {
-    //
-    //     var self = this;
-    //
-    //     self.textSelect.val(cookie.getCookie('app-text'));
-    //
-    //     self.textSelect.on('change', function() {
-    //
-    //         cookie.setCookie('app-text', $(this).val(), COOKIE_PATH);
-    //         self.setApplicationStyles();
-    //
-    //     });
-    //
-    // }
-    //
-    // handleColorChange() {
-    //
-    //     var self = this;
-    //
-    //     self.colorSelect.val(cookie.getCookie('app-color'));
-    //
-    //     self.colorSelect.on('change', function() {
-    //
-    //         cookie.setCookie('app-color', $(this).val(), COOKIE_PATH);
-    //         self.setApplicationStyles();
-    //
-    //     });
-    //
-    // }
-    //
-    // handleImageChange() {
-    //
-    //     var self = this;
-    //
-    //     self.imageSelect.val(cookie.getCookie('app-image'));
-    //
-    //     self.imageSelect.on('change', function() {
-    //
-    //         cookie.setCookie('app-image', $(this).val(), COOKIE_PATH);
-    //         self.setApplicationStyles();
-    //
-    //         self.toggleImage();
-    //
-    //     });
-    //
-    //
-    //
-    // }
-    //
-    // wrapImageAlt() {
-    //
-    //     $('img').each(function() {
-    //
-    //         var alt = $(this).attr('alt');
-    //
-    //         if (alt != '') {
-    //             $(this).wrap(`<span class="image-alt"/>`);
-    //             $(this).after(`<span class="image-alt__text">${alt}</span>`);
-    //         } else {
-    //             $(this).wrap(`<span class="image-alt image-alt_empty"/>`);
-    //         }
-    //
-    //     });
-    //
-    // }
-    //
-    // toggleImage() {
-    //
-    //     if (cookie.getCookie('app-image') == 'hidden') {
-    //         $('.image-alt').addClass('image-alt_active');
-    //         $('img').css('visibility', 'hidden');
-    //     } else {
-    //         $('.image-alt').removeClass('image-alt_active');
-    //         $('img').css('visibility', 'visible');
-    //     }
-    //
-    // }
-    //
-    // setApplicationStyles() {
-    //
-    //     this.app.attr('data-text', cookie.getCookie('app-text'));
-    //     this.app.attr('data-color', cookie.getCookie('app-color'));
-    //     this.app.attr('data-image', cookie.getCookie('app-image'));
-    //
-    // }
 }
 
 export default new ControlPanel();
